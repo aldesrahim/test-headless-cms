029127fef59d7d46af251a90feaaa267a9c1f29c
aldesrahim <aldesrahim@gmail.com>
2025-06-29 17:30:42 +0700

    feat: add flash-alert component

---
+++ resources/views/components/flash-alert.blade.php
@@ -0,0 +1,60 @@
+<div
+    x-data="{
+        isShow: false,
+        alert: null,
+        init() {
+            window.addEventListener('flash-alert:show', event => {
+                const detail = Array.isArray(event.detail) ? event.detail[0] : event.detail;
+
+                let content = null;
+                let duration = null;
+
+                if (typeof detail === 'object') {
+                    content = detail.content;
+                    duration = detail.duration ?? null;
+                } else {
+                    content = detail;
+                }
+
+                if (!this.alert) {
+                    this.alert = {
+                        content,
+                        duration: duration ?? 5000,
+                        timeout: null,
+                    };
+
+                    this.show();
+                }
+            });
+        },
+        show() {
+            if (!this.alert) {
+                return;
+            }
+
+            this.isShow = true;
+
+            this.alert.timeout = setTimeout(() => {
+                this.hide();
+            }, this.alert.duration);
+        },
+        hide() {
+            this.isShow = false;
+
+            if (this.alert != null) {
+                clearTimeout(this.alert.timeout);
+            }
+
+            this.alert = null;
+        }
+    }"
+>
+    <div
+        class="flex mb-5 pl-5 pr-3 py-2 items-center rounded space-x-2 [print-color-adjust:exact] bg-zinc-800/5 dark:bg-white/10"
+        x-cloak
+        x-show="isShow"
+    >
+        <span class="grow" x-text="alert?.content"></span>
+        <flux:button icon="x-mark" variant="subtle" @click="hide()" />
+    </div>
+</div>
