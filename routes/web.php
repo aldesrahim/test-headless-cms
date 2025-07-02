8ec0543048f64655b7f09a7ced64e210cf695c0a
aldesrahim <aldesrahim@gmail.com>
2025-07-02 13:59:47 +0700

    feat(chore): remove register route and default welcome page

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::redirect('/', '/dashboard')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Volt::route('dashboard', 'dashboard')->name('dashboard');
